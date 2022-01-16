8b635684dcb8bc0e52959ebeb4bad4a966e3abdf
Damian Osypinski <d.osypinski@gmail.com>
2022-01-16 14:36:25 +0100

    add initActions()

--- src/js/script.js
+++ src/js/script.js
@@ -6,26 +6,48 @@
   const select = {
     bookList: '.books-list',
     templateOf: '#template-book',
+    bookImage: '.book__image'
   };
-  const tplSource = document.querySelector(select.templateOf).innerHTML;
-  const tpl = Handlebars.compile(tplSource);
+  const tpl = Handlebars.compile(document.querySelector(select.templateOf).innerHTML);
+
+
 
   function render() {
     const thisBook = this;
 
     for(let book of dataSource.books){
-      const bookData = {
-        id: book.id,
-        name: book.name,
-        price: book.price,
-        rating: book.rating,
-        image: book.image,
-      };
-      const generatedHTML = tpl(bookData);
+
+      const generatedHTML = tpl(book);
       thisBook.element = utils.createDOMFromHTML(generatedHTML);
       const bookListContainer = document.querySelector(select.bookList);
       bookListContainer.appendChild(thisBook.element);
     }
   }
+
+
+  function initActions() {
+    const thisBook = this;
+
+    thisBook.favoriteBooks = [];
+    thisBook.booklist = document.querySelector(select.bookList);
+    thisBook.booklist.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      const image = event.target.offsetParent;
+      const idBook = image.getAttribute('data-id');
+      if(thisBook.favoriteBooks.includes(idBook)){
+        image.classList.remove('favorite');
+        thisBook.favoriteBooks.pop(idBook);
+      }
+      else{
+        image.classList.add('favorite');
+        thisBook.favoriteBooks.push(idBook);
+      }
+      console.log('favoriteBooks', thisBook.favoriteBooks);
+    });
+    
+
+  }
+
   render();
+  initActions();
 }
